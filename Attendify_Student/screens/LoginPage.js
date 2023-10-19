ea5ffdf8f528a81c5d2a96ec23c4c766eddbfb8f
Kolyn090 <78561131+Kolyn090@users.noreply.github.com>
2023-10-18 22:59:48 -0400

    Changed style structures

--- Attendify_Student/screens/LoginPage.js
+++ Attendify_Student/screens/LoginPage.js
@@ -1,23 +1,27 @@
 import * as React from 'react';
+import {useState, useCallback} from 'react';
 import { Platform } from 'react-native';
 import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { SafeAreaView, Image, Dimensions } from 'react-native';
 import Checkbox from 'expo-checkbox';
-import {useState, useEffect, useCallback} from 'react';
-import * as Font from "expo-font";
 import * as SplashScreen from 'expo-splash-screen';
-import { FontFamily, FontSize, Color } from '../style/GlobalStyle';
-import { AppTheme } from '../style/AppTheme';
+import { currentTheme, changeTheme } from '../style/AppTheme';
+import { loadFont } from '../props/FontLoader';
+import { kolynScreen, kolynInputfield, kolynButton, kolynLabel } from '../style/KolynKit';
 
+changeTheme(0);
 const ios = Platform.OS == 'ios';
 
 export function LoginPage(props) {
+  /* The 'emailText' variable will be modified by the user */
   const [emailText, onChangeEmailText] = React.useState('');
+  /* The 'passwordText' variable will be modified by the user */
   const [passwordText, onChangePasswordText] = React.useState('');
   const { onPress = 'Save' } = props;
+  /* The 'isChecked' turns to true if the box if checked, otherwise false */
   const [isChecked, setChecked] = useState(false);
 
-  const fontsLoaded = LoadFont();
+  const fontsLoaded = loadFont();
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
       await SplashScreen.hideAsync();
@@ -29,7 +33,7 @@ export function LoginPage(props) {
   }
 
   return (
-    <View style={styles.container}
+    <View style={styles.screen}
         onLayout={onLayoutRootView}>
       <SafeAreaView 
         className={ios? '-mb-8': ''}
@@ -54,193 +58,149 @@ export function LoginPage(props) {
             setChecked = {setChecked}
             isChecked = {isChecked}
           />
-
-          <SignupButton onPress={onPress} />
         </View>
-        <Credits/>
+        <View style={{flex: 1}}>
+          <SignupButton onPress={onPress} />
+          <Credits/>
+        </View>
       </SafeAreaView>
     </View>
   );
 }
 
-function LoadFont() {
-  const [fontsLoaded] = Font.useFonts({
-    'BalooBhai-Regular': require('../assets/fonts/BalooBhai-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    async function prepare() {
-      await SplashScreen.preventAutoHideAsync();
-    }
-  
-    prepare();
-  }, []);
-
-  return fontsLoaded;
-}
-
+/* The icon & title image */
 function MainTitleImage() {
-  const {width} = Dimensions.get('window');
-
   return (
     <Image
       source={require('../assets/main-title.png')}
-      style={{
-        resizeMode: 'contain',
-        width: width*3.0/2.0,
-        height: width*0.2,
-        alignSelf: 'center',
-      }}
+      style={styles.title}
     />
   )
 }
 
+/* The email textfield */
+function EmailTextfield({ onChangeEmailText, emailText }) {
+  return (
+    <TextInput
+      style={styles.inputTextfield}
+      onChangeText={onChangeEmailText}
+      value={emailText}
+      placeholder="Enter email"
+      keyboardType="email-address"
+      secureTextEntry={false}
+    />
+  )
+}
+
+/* The password textfield, secure typing */
+function PasswordTextfild({ onChangePasswordText, passwordText }) {
+  return (
+    <TextInput
+      style={styles.inputTextfield}
+      onChangeText={onChangePasswordText}
+      value={passwordText}
+      placeholder="Enter password"
+      keyboardType="default"
+      secureTextEntry={true}
+    />
+  )
+}
+
+/* The login button */
+function LoginButton({ onPress }) {
+  return (
+    <Pressable style={styles.loginButton} onPress={onPress}>
+      <Text style={styles.loginButtonLabel}>Log in</Text>
+    </Pressable>
+  )
+}
+
+/* Remember me check-in box and label */
+function RememberMe({ setChecked, isChecked }) {
+  return (
+    <View style={styles.rememberMe}>
+      <Checkbox 
+        style={styles.checkbox}
+        onValueChange={setChecked}
+        color={isChecked ? currentTheme.checkBoxColor : currentTheme.subColor}
+        value={isChecked}/>
+      <Text style={styles.rememberMeLabel}>Remember me</Text>
+    </View>
+  )
+}
+
+/* The signup button */
+function SignupButton({ onPress }) {
+  return (
+    <Pressable style={[
+      styles.signupButton]}
+      onPress={onPress}>
+      <Text style={styles.signupButtonLabel}>Sign up</Text>
+    </Pressable>
+  )
+}
+
+/* The credits label */
 function Credits({  }) {
   return (
-    <Text style={{
-      top: 0,
-      fontSize: FontSize.tiny,
-      fontFamily: FontFamily.mainFont,
-      color: Color.colorWhite,
-      textAlign: 'center',
-      flex: 1,
-    }}>
+    <Text style={styles.creditLabel}>
       Credits: Proud app made by CS 320 Group 6
     </Text>
   )
 }
 
-function SignupButton({ onPress }) {
-  return (
-    <Pressable style={[
-      {top: 250},
-      styles.smallButton]}
-      onPress={onPress}>
-      <Text style={{
-        fontSize: FontSize.small,
-        fontFamily: FontFamily.mainFont,
-        color: AppTheme.theme,
-      }}>Sign up</Text>
-    </Pressable>
-  )
-}
-
-function RememberMe({ setChecked, isChecked }) {
-  return (
-    <View style={{
-      top: 70,
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}>
-      <Checkbox 
-        style={styles.checkbox}
-        onValueChange={setChecked}
-        color={isChecked ? 'green' : Color.colorWhite}
-        value={isChecked}
-        >
-      </Checkbox>
-
-      <Text style={{
-        fontSize: FontSize.medium,
-        fontFamily: FontFamily.mainFont,
-        color: Color.colorWhite,
-      }}>Remember me</Text>
-    </View>
-  )
-}
-
-function LoginButton({ onPress }) {
-  return (
-    <Pressable style={styles.loginButton} onPress={onPress}>
-      <Text style={{
-        fontSize: FontSize.casual,
-        fontFamily: FontFamily.mainFont,
-        color: AppTheme.theme,
-      }}>Log in</Text>
-    </Pressable>
-  )
-}
-
-function EmailTextfield({ onChangeEmailText, emailText }) {
-  return (
-    <Textfield
-      onChangeText={onChangeEmailText}
-      value={emailText}
-      placeholder="Enter email"
-      keyboardType="email-address"
-      isSecure={false}
-    />
-  )
-}
-
-function PasswordTextfild({ onChangePasswordText, passwordText }) {
-  return (
-    <Textfield
-      onChangeText={onChangePasswordText}
-      value={passwordText}
-      placeholder="Enter password"
-      keyboardType="default"
-      isSecure={true}
-    />
-  )
-}
-
-function Textfield({onChangeText, value, placeholder, keyboardType, isSecure}) {
-  return (
-    <TextInput 
-      secureTextEntry={isSecure}
-      style={styles.input}
-      onChangeText={onChangeText}
-      value={value}
-      placeholder={placeholder}
-      keyboardType={keyboardType}
-    />
-  )
-}
-
+const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 24,
-    backgroundColor: AppTheme.theme,
-  },
 
-  input: {
-    height: 40,
-    margin: 12,
-    backgroundColor: Color.colorWhite,
-    padding: 10,
-    borderRadius: 10,
-    fontFamily: FontFamily.mainFont,
-  },
+  screen: StyleSheet.flatten([
+    kolynScreen(currentTheme.mainColor),
+  ]),
 
-  loginButton: {
-    top: 55,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 1,
-    paddingHorizontal: 1,
-    borderRadius: 10,
-    elevation: 3,
-    width: 240,
-    backgroundColor: Color.colorWhite,
+  title: {
+    resizeMode: 'contain',
+    width: 400,
+    height: width*0.2,
     alignSelf: 'center',
   },
 
-  smallButton: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 1,
-    paddingHorizontal: 1,
-    borderRadius: 10,
-    elevation: 3,
-    width: 70,
-    backgroundColor: Color.colorWhite,
-    alignSelf: 'center',
+  inputTextfield: StyleSheet.flatten([
+    {height: 40, width: 300}, 
+    kolynInputfield(currentTheme.subColor, currentTheme.mainFont),
+  ]),
+
+  loginButton: StyleSheet.flatten([
+    {top: 55, width: 240}, 
+    kolynButton(currentTheme.subColor),
+  ]),
+
+  loginButtonLabel: StyleSheet.flatten([
+    kolynLabel(currentTheme.fontSizes.casual, currentTheme.mainFont, currentTheme.mainColor)
+  ]),
+
+  rememberMe: {
+    top: 70,
+    flexDirection: 'row',
+    alignSelf:'center',
   },
 
-  checkbox: {
-    margin: 8,
-  }
+  checkbox: { margin: 8 },
+
+  rememberMeLabel: StyleSheet.flatten([
+    kolynLabel(currentTheme.fontSizes.medium, currentTheme.mainFont, currentTheme.subColor),
+  ]),
+
+  signupButton: StyleSheet.flatten([
+    {height: 40, width: 70},
+    kolynButton(currentTheme.subColor),
+  ]),
+
+  signupButtonLabel: StyleSheet.flatten([
+    kolynLabel(currentTheme.fontSizes.small, currentTheme.mainFont, currentTheme.mainColor,),
+  ]),
+
+  creditLabel: StyleSheet.flatten([
+    {textAlign: 'center'},
+    kolynLabel(currentTheme.fontSizes.tiny, currentTheme.mainFont, currentTheme.subColor),
+  ]),
+
 });
