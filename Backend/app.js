6a9367a12e8936d459ba88ad3036ac540c1028dc
ishan2017 <ishan.keezhakada@gmail.com>
2023-10-29 14:18:59 -0400

    Added Cors

--- Backend/app.js
+++ Backend/app.js
@@ -7,9 +7,12 @@ const loginResource = require("./API/resources/login");
 const registerResource = require("./API/resources/register");
 const qrReasource = require('./API/resources/qr');
 const morgan = require("morgan");
+const cors = require("cors");
 const dbConnect = require("./db/dbConnect");
+
 dbConnect();
 app.use(morgan('dev'));
+app.use('cors');
 app.use("/student", studentResource);
 app.use("/instructor", instructorResource);
 app.use("/course", courseResource);
