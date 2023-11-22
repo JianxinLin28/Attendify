fdbf67296838f72c44fa252ab92ff4032efd8ece
Powall <powallwang@gmail.com>
2023-11-21 23:19:08 -0500

    login response change and create course register section

---
+++ Backend/API/controllers/courseRegistrationController.js
@@ -0,0 +1,15 @@
+const User = require("../models/courseRegistrationModel.js");
+
+const get = async (request, response) => {
+ 
+};
+
+const edit = async (request, response) => {
+  
+};
+
+const del = (request, response) => {
+  
+};
+
+module.exports = {get, edit, del};
