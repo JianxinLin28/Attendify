4f533b64a442bdeb958ed22a49e65ad274926a7b
Zhitong <zhitongl@umass.edu>
2023-12-07 13:58:33 -0500

    qr-code

--- src/components/QRModal.js
+++ src/components/QRModal.js
@@ -1,65 +1,78 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import QR from '../constants/qr.png';
+import React, { useState } from 'react';
+import axios from 'axios';
+import QRCode from 'qrcode';
 import Swal from 'sweetalert2';
+import Button from '@mui/material/Button';
 
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+const QRCodeGenerator = () => {
+  const [qrCodeImage, setQRCodeImage] = useState(null);
 
-export default function BasicModal() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  <style jsx>{`
-    .modal-image {
-      width: 250px;
-      height: auto;
-      draggable = false;
+  const generateQRCode = async (data) => {
+    try {
+      console.log(data);
+      const qrCode = await QRCode.toDataURL(data);
+      setQRCodeImage(qrCode);
+      return qrCode; // Return the QR code to be used in the then callback
+    } catch (error) {
+      console.error('Error generating QR code:', error.message);
+      throw error; // Re-throw the error to be caught in the caller
     }
-  `}</style>
+  };
 
-  const qrButton = () => {
-    Swal.fire({
-      title: "Attendance QR Code",
-      text: "Scan the QR code to mark your attendance!",
-      imageUrl: QR,
-      imageWidth: 300,
-      imageHeight: 300,
-      imageAlt: "QR-Image"
+  const sendPostRequestAndGenerateQRCode = async (courseId) => {
+    try {
+      // Update the backend URL to http://localhost:8080/qr
+      const backendURL = 'http://localhost:8080/qr';
+
+      // Make a POST request using Axios with the specified courseId
+      const response = await axios.post(backendURL, { course_id: courseId });
+
+      // Log the response from the backend
+      console.log('Response from backend:', response.data);
+
+      // Extract the token from the "key" attribute in the response
+      const keyAttribute = 'key';
+      const token = response.data[keyAttribute];
+
+      console.log(`Token extracted from ${keyAttribute}:`, token);
+
+      // Generate a QR code with the extracted token
+      generateQRCode(JSON.stringify(token)).then((qrCode) => {
+        // Display the QR code in the SweetAlert2 modal
+        Swal.fire({
+          title: 'Scan for attendance',
+          html: `<img src="${qrCode}" alt="QR Code" draggable="false" style="display:block;margin:auto;height:300px;width:300px;"/>`,
+          confirmButtonColor: '#3085d6',
+        });
+      });
+
+    } catch (error) {
+      console.error('Error sending POST request:', error.message);
+    }
+  };
+
+  const handleClick = async () => {
+    const { value: courseId } = await Swal.fire({
+      title: 'Enter Course ID',
+      input: 'text',
+      inputLabel: 'Course ID',
+      inputPlaceholder: 'Enter the course ID',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'QR Code',
     });
-  }
+
+    if (courseId) {
+      sendPostRequestAndGenerateQRCode(courseId);
+    }
+  };
 
   return (
     <div>
-      <Button onClick={qrButton}>QR Code</Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Scan QR Code for attendance!
-          </Typography>
-          <Typography id="modal-modal-description modal-image" sx={{ mt: 2}}>
-            <img src={QR} sx={{height: 3}} alt="QR Code" />
-          </Typography>
-        </Box>
-      </Modal>
+      <Button onClick={handleClick}>QR Code</Button>
     </div>
   );
-}
+};
+
+export default QRCodeGenerator;
